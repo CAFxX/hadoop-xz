a5e48a4755964a47b509eaebaa56fc806745f20c
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2017-07-31 12:33:04 +0900

    Update XZCompressionInputStream.java

--- src/main/java/io/sensesecure/hadoop/xz/XZCompressionInputStream.java
+++ src/main/java/io/sensesecure/hadoop/xz/XZCompressionInputStream.java
@@ -18,45 +18,34 @@ public class XZCompressionInputStream extends CompressionInputStream {
 
     private XZInputStream xzIn;
 
-    private boolean resetStateNeeded;
-
     public XZCompressionInputStream(InputStream in) throws IOException {
         super(in);
-        resetStateNeeded = false;
-        bufferedIn = new BufferedInputStream(super.in);
+        resetState();
     }
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-        if (resetStateNeeded) {
-            resetStateNeeded = false;
-            bufferedIn = new BufferedInputStream(super.in);
-            xzIn = null;
-        }
         return getInputStream().read(b, off, len);
     }
 
     @Override
     public void resetState() throws IOException {
-        resetStateNeeded = true;
+        bufferedIn = new BufferedInputStream(super.in);
+        xzIn = null;
     }
 
     @Override
     public int read() throws IOException {
-        byte b[] = new byte[1];
-        int result = this.read(b, 0, 1);
-        return (result < 0) ? result : (b[0] & 0xff);
+        return getInputStream().read();
     }
 
     @Override
     public void close() throws IOException {
-        if (!resetStateNeeded) {
-            if (xzIn != null) {
-                xzIn.close();
-                xzIn = null;
-            }
-            resetStateNeeded = true;
+        if (xzIn != null) {
+            xzIn.close();
+            xzIn = null;
         }
+        resetState();
     }
 
     /**
